eedb4a0ac7a8e2d9c1205a6ca82e0e0fd7b8e2d0
Apop85 <rbaldinger@gmx.ch>
2019-08-24 20:35:19 +0200

    Lernkarten Update
    Einschränkung der Zufallsfrage anhand Punktezahl

--- random.php
+++ random.php
@@ -1,11 +1,17 @@
 <?php
-    function get_random_card() {
+    function get_random_card($max_score=999999999) {
         # Zufällige Karte auswählen
+        $search = false;
         $emptyarray = [];
         $cards = get_cards('./cards/', $emptyarray);
-        $random = mt_rand(0, sizeof($cards)-1);
-        include($cards[$random]);
-        return array($q,$a,$f,$cards[$random]);
+        while (!$search) {
+            $random = mt_rand(0, sizeof($cards)-1);
+            include($cards[$random]);
+            if ($s <= $max_score) {
+                return array($q,$a,$f,$cards[$random]);
+            }
+        }
+        // return array($q,$a,$f,$cards[$random]);
     }
     
     function get_cards($main, $cards){
@@ -26,13 +32,13 @@
         }
         return $cards;
     }
-    get_random_card();
+    // get_random_card(999999999);
     
     function check_answer($card) {
         # Prüfe Antwort
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $antwort = strtolower(test_input($_POST["answer"]));
-            $test = strtolower(test_input($_POST["true_answer"]));
+            $antwort = htmlspecialchars(strtolower(test_input($_POST["answer"])));
+            $test = htmlspecialchars(strtolower(test_input($_POST["true_answer"])));
             $last_file = test_input($_POST["last_file"]);
             
             if ($antwort == $test){
@@ -42,7 +48,7 @@
                 return $antwortfeld.$output;
             }
             else {
-                $antwortfeld = '<div class="false_value">Diese Antwort war falsch!! Die korrekte Antwort lautet:</br><div class="correct">'.$test.'</div>Siehe '.$card[2].'</div>';
+                $antwortfeld = '<div class="false_value">Diese Antwort war falsch!! Die korrekte Antwort lautet:</br><div class="correct">'.htmlspecialchars($test).'</div>Siehe '.$card[2].'</div>';
                 $command = escapeshellcmd('python ./py/update_score.py  "update" "-1" "'.$last_file.'"');
                 $output = shell_exec($command);
                 return $antwortfeld.$output;
@@ -50,6 +56,20 @@
         }
     }
     
+    function max_score_value() {
+        $output =   "<a>Maximale Punktzahl: </a><select name='max_score'>
+                    <option value='ALLE'>ALLE</options>";
+        $start_value = 10;
+        for ($i=0; $i <= 10; $i++) {
+            $output .= '<option value="'.$start_value.'">'.$start_value.'</option>';
+            $start_value--;
+        }
+        $output .= '</select>';
+
+        return $output;
+    }
+
+
     function test_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
@@ -61,6 +81,16 @@
 <?php
     $init_form = '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">';
     $titel = 'Zufallsfrage';
+    if (key_exists("max_score", $_POST)) {
+        $max_score = $_POST["max_score"];
+        if ($max_score == "ALLE") {
+            $card = get_random_card(9999999);
+        } else {
+            $card = get_random_card($max_score);
+        }
+    } else {
+        $card = get_random_card(9999999);
+    }
     $card = get_random_card();
     $won = check_answer($card);
     $header = '<p class="content_title">Eine beliebige Zufallsfrage</p><p class="content_text">Eine Zufällige Frage aus der Kartei wird ausgewählt und ausgegeben.</p>';
@@ -69,14 +99,17 @@
     $footer = '</div></div>';
     $true_answer = '<input type="hidden" name="true_answer" value="'.$card[1].'"><input type="hidden" name="last_file" value="'.$card[3].'">';
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $max_score = $_POST["max_score"];
+        $transmitter = '<input type="hidden" name="max_score" value="'.$max_score.'">';
         $divs = '<div class="flip-card"><div class="flip-card-inner">';
         $frage = '<div class="flip-card-back"><p class="frage">'.$card[0].'</p>';
-        $content = $header.$divs.$frage.$init_form.$button.$validation.$footer.$true_answer.'</form>';
+        $content = $header.$divs.$frage.$init_form.$transmitter.$button.$validation.$footer.$true_answer.'</form>';
     }
     else {
+        $max_score_menu = max_score_value();
         $divs = '<div class="flip-card"><div class="flip-card-static">';
         $frage = '<div class="flip-card-b-static"><p class="frage">'.$card[0].'</p>';
-        $content = $header.$divs.$frage.$init_form.$button.$footer.$true_answer.'</form>';
+        $content = $header.$divs.$frage.$init_form.$max_score_menu.$button.$footer.$true_answer.'</form>';
     }
 ?>
 
